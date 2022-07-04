7325ab56af4def83fafe95e7d0c42b742733d075
SimonLab <simon@dwyl.com>
2022-07-04 16:27:10 +0100

    Add tests linked to authentication
    Add tests for session

--- lib/labels_web/router.ex
+++ lib/labels_web/router.ex
@@ -12,10 +12,6 @@ defmodule LabelsWeb.Router do
     plug :assign_github_token
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   pipeline :auth do
     plug :authenticate
   end
@@ -32,40 +28,6 @@ defmodule LabelsWeb.Router do
 
     get "/", PageController, :index
     delete "/logout", GithubAuthController, :logout
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", LabelsWeb do
-  #   pipe_through :api
-  # end
-
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
-
-    scope "/" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: LabelsWeb.Telemetry
-    end
-  end
-
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through :browser
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
   end
 
   defp assign_github_token(conn, _opts) do
--- test/labels_web/controllers/page_controller_test.exs
+++ test/labels_web/controllers/page_controller_test.exs
@@ -1,8 +1,31 @@
 defmodule LabelsWeb.PageControllerTest do
   use LabelsWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET / redirect to login when not logged in", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert redirected_to(conn, 302) =~ "/login"
+  end
+
+  test "GET / diplay page when logged in", %{conn: conn} do
+    conn =
+      conn
+      |> Plug.Test.init_test_session(github_token: "123")
+      |> get("/")
+
+    assert html_response(conn, 200)
+  end
+
+  test "DELETE /logout page redirect when not logged in", %{conn: conn} do
+    conn = delete(conn, "/logout")
+    assert redirected_to(conn, 302) =~ "/login"
+  end
+
+  test "DELETE /logout redirect also to login once logged out", %{conn: conn} do
+    conn =
+      conn
+      |> Plug.Test.init_test_session(github_token: "123")
+      |> delete("/logout")
+
+    assert redirected_to(conn, 302) =~ "/login"
   end
 end
