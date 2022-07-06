5ca2920cedd2bf2646936a8876f728c7deb40a79
SimonLab <simon@dwyl.com>
2022-07-06 10:48:00 +0100

    Add form for sync labels
    Create form to display source and target inputs

--- lib/labels_web/controllers/page_controller.ex
+++ lib/labels_web/controllers/page_controller.ex
@@ -5,12 +5,31 @@ defmodule LabelsWeb.PageController do
   Render main application page
   """
   def index(conn, _params) do
-    labels = github_api().get_labels(conn.assigns.github_token, "dwyl", "labels")
+    render(conn, "index.html")
+  end
+
+  def sync(conn, %{"sync_labels" => form}) do
+    {:ok, source_labels} =
+      github_api().get_labels(
+        conn.assigns.github_token,
+        form["source_owner"],
+        form["source_repo"]
+      )
+
+    {:ok, target_labels} =
+      github_api().get_labels(
+        conn.assigns.github_token,
+        form["target_owner"],
+        form["target_repo"]
+      )
+
     # Enum.each(labels, fn label ->
     #   github_api().create_label(conn.assigns.github_token, "simonLab", "time-mvp", label)
     # end)
 
-    render(conn, "index.html", labels: labels)
+    conn
+    |> put_flash(:info, "Labels synced!")
+    |> redirect(to: Routes.page_path(conn, :index))
   end
 
   defp github_api, do: Application.get_env(:labels, :github_api)
