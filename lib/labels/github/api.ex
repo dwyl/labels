d3029c59d61441ccb42829bbfc6a91781de0bf6e
SimonLab <simon@dwyl.com>
2022-12-08 10:16:49 +0000

    Add user_agent
    Add user_agent value in api requests

--- lib/labels/github/api.ex
+++ lib/labels/github/api.ex
@@ -15,7 +15,11 @@ defmodule Labels.Github.Api do
     req = Req.new(base_url: "https://api.github.com")
 
     res =
-      Req.get!(req, url: "/repos/#{owner}/#{repo}/labels?per_page=100", auth: {:bearer, token})
+      Req.get!(req,
+        url: "/repos/#{owner}/#{repo}/labels?per_page=100",
+        auth: {:bearer, token},
+        user_agent: "dwyl-labels"
+      )
 
     check_api_response(res)
   end
@@ -25,7 +29,8 @@ defmodule Labels.Github.Api do
     res =
       Req.post!("https://api.github.com/repos/#{owner}/#{repo}/labels",
         auth: {:bearer, token},
-        json: label
+        json: label,
+        user_agent: "dwyl-labels"
       )
 
     Logger.info("create label: #{label["name"]}, response status: #{res.status}")
@@ -39,7 +44,8 @@ defmodule Labels.Github.Api do
     res =
       Req.patch!(endpoint,
         auth: {:bearer, token},
-        json: label
+        json: label,
+        user_agent: "dwyl-labels"
       )
 
     Logger.info("update label: #{label["name"]}, response status: #{res.status}")
@@ -48,8 +54,14 @@ defmodule Labels.Github.Api do
 
   defp check_api_response(res) do
     case res.status do
+      # label updated
       200 -> {:ok, res.body}
+      # label created
+      201 -> {:ok, res.body}
       404 -> {:error, :not_found}
+      # see https://docs.github.com/en/rest/overview/resources-in-the-rest-api?apiVersion=2022-11-28#failed-login-limit
+      401 -> {:error, :unauthorized}
+      403 -> {:error, :forbidden}
       _ -> {:error, :unknown}
     end
   end
